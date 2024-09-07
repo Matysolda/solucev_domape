287d89f50de8e6501e36d933ad9907f1858947c4
Matysolda <157897479+Matysolda@users.noreply.github.com>
2024-09-07 22:12:42 +0200

    Update script.js

--- www/script.js
+++ www/script.js
@@ -1,10 +1,49 @@
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav-links');
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('.nav-links a');
+    const header = document.querySelector('header');
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
 
-burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
-    burger.classList.toggle('toggle');
+    // Smooth scroll and close menu for smaller screens
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href');
+            const targetSection = document.querySelector(targetId);
+
+            // Odebere "active" třídu ze všech položek
+            navLinks.forEach(navLink => navLink.parentElement.classList.remove('active'));
+
+            // Přidá "active" třídu k aktuální položce
+            this.parentElement.classList.add('active');
+
+            if (targetSection) {
+                const headerHeight = header.offsetHeight;
+                const targetPosition = targetSection.offsetTop - headerHeight;
+
+                window.scrollTo({
+                    top: targetPosition,
+                    behavior: 'smooth'
+                });
+
+                // Close menu after click
+                nav.classList.remove('nav-active');
+                burger.classList.remove('toggle');
+            }
+        });
+    });
+
+    // Toggle burger menu
+    burger.addEventListener('click', function () {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    });
 });
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const navLinks = document.querySelectorAll('.nav-links a'); // Výběr všech odkazů v navigaci
     const header = document.querySelector('header'); // Výběr pevného menu
